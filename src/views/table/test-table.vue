54f7ce44c9fe0a6cce308086f6b226c6981c040c
JianLang Huang <jianlang.huang@ibm.com>
2021-04-27 17:59:48 +0800

    修改，删除功能

--- src/views/table/test-table.vue
+++ src/views/table/test-table.vue
@@ -1,18 +1,41 @@
 <template>
-  <div>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-input v-model="listQuery.title" :placeholder="$t('table.title')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-select v-model="listQuery.importance" :placeholder="$t('table.importance')" clearable style="width: 120px" class="filter-item">
+        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
+      </el-select>
+      <el-select v-model="listQuery.type" :placeholder="$t('table.type')" clearable class="filter-item" style="width: 130px">
+        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.displayName+'('+item.key+')'" :value="item.key" />
+      </el-select>
+      <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
+        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
+      </el-select>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        {{ $t('table.search') }}
+      </el-button>
+      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate(updateReleaseDia = true)">
+        {{ $t('table.add') }}
+      </el-button>
+      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+        {{ $t('table.export') }}
+      </el-button>
+    </div>
     <el-table :data="list" fit highlight-current-row style="width: 100%">
       <el-table-column type=index prop="CRM_CODE" :label="$t('table.CRM_CODE')" align="center" width="95"></el-table-column>
       <el-table-column prop="CRM_TYPE" :label="$t('table.CRM_TYPE')" align="center" class-name="" width="110"></el-table-column>
       <el-table-column prop="CCY_CODE" :label="$t('table.CCY_CODE')" align="center"></el-table-column>
-      <el-table-column prop="CRM_VALUE_ORI" :label="$t('table.CRM_VALUE_ORI')" align="center"></el-table-column>
-      <el-table-column prop="CRM_CP_TYPE" :label="$t('table.CRM_CP_TYPE')" align="center"></el-table-column>
+      <el-table-column prop="CRM_VALUE_ORI" :label="$t('table.CRM_VALUE_ORI')" align="center" width="120"></el-table-column>
+      <el-table-column prop="CRM_CP_TYPE" :label="$t('table.CRM_CP_TYPE')" align="center" width="120"></el-table-column>
       <el-table-column prop="CRM_CPC" :label="$t('table.CRM_CPC')" align="center" width="180"></el-table-column>
-      <el-table-column prop="CRM_ST_RATING" :label="$t('table.CRM_ST_RATING')" align="center"></el-table-column>
-      <el-table-column prop="CRM_LT_RATING" :label="$t('table.CRM_LT_RATING')" align="center"></el-table-column>
+      <el-table-column prop="CRM_ST_RATING" :label="$t('table.CRM_ST_RATING')" align="center" width="110"></el-table-column>
+      <el-table-column prop="CRM_LT_RATING" :label="$t('table.CRM_LT_RATING')" align="center" width="110"></el-table-column>
       <el-table-column :label="$t('table.actions')" width="180">
         <template slot-scope="scope">
           <el-button type="warning" size="mini" @click="updateDialogClosed(scope.row)">
-          {{$t('table.update')}}</el-button>
+            {{$t('table.update')}}</el-button>
+          <el-button type="danger" size="mini" @click="deleteRow(scope.$index)">
+            {{$t('table.delete')}}</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -24,21 +47,20 @@
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_TYPE')" required>
               <el-select v-model="form.CRM_TYPE">
-              <el-option v-for="item in CRM_TYPE" :key="item"  :label="item"
-      :value="item">
-              </el-option>
+                <el-option v-for="item in CRM_TYPE" :key="item" :label="item" :value="item">
+                </el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item :label="$t('table.startDate')" required label-width='150px'>
-              <el-date-picker  v-model="startDate" type="date" placeholder="选择日期">
+              <el-date-picker v-model="startDate" type="date" placeholder="选择日期">
               </el-date-picker>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item :label="$t('table.endDate')" required>
-              <el-date-picker v-model="endDate"  type="date" placeholder="选择日期">
+              <el-date-picker v-model="endDate" type="date" placeholder="选择日期">
               </el-date-picker>
             </el-form-item>
           </el-col>
@@ -46,19 +68,18 @@
       </el-form>
       <el-form inline label-width='130px' label-position="left">
         <el-row>
-        <el-col :span="8">
-          <el-form-item :label="$t('table.CCY_CODE')" required>
-            <el-select v-model="form.CCY_CODE">
-              <el-option v-for="item in CCY_CODE" :key="item"  :label="item"
-      :value="item"></el-option>
-            </el-select>
-          </el-form-item>
-        </el-col>
-        <el-col :span="16">
-          <el-form-item :label="$t('table.CRM_VALUE_ORI')" required label-width='150px'>
-            <el-input type="number" v-model="form.CRM_VALUE_ORI"></el-input>
-          </el-form-item>
-        </el-col>
+          <el-col :span="8">
+            <el-form-item :label="$t('table.CCY_CODE')" required>
+              <el-select v-model="form.CCY_CODE">
+                <el-option v-for="item in CCY_CODE" :key="item" :label="item" :value="item"></el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span="16">
+            <el-form-item :label="$t('table.CRM_VALUE_ORI')" required label-width='150px'>
+              <el-input type="number" v-model="form.CRM_VALUE_ORI"></el-input>
+            </el-form-item>
+          </el-col>
         </el-row>
       </el-form>
       <el-form inline label-width='130px' label-position="left">
@@ -66,24 +87,21 @@
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_CP_TYPE')" required>
               <el-select v-model="form.CRM_CP_TYPE">
-                <el-option v-for="item in CRM_CP_TYPE" :key="item"  :label="item"
-      :value="item"></el-option>
+                <el-option v-for="item in CRM_CP_TYPE" :key="item" :label="item" :value="item"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_CPC')" required label-width='150px'>
               <el-select v-model="form.CRM_CPC">
-                <el-option v-for="item in CRM_CPC" :key="item"  :label="item"
-      :value="item"></el-option>
+                <el-option v-for="item in CRM_CPC" :key="item" :label="item" :value="item"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_CP_RATING')">
               <el-select v-model="form.CRM_CP_RATING">
-                <el-option v-for="item in CRM_CP_RATING" :key="item"  :label="item"
-      :value="item"></el-option>
+                <el-option v-for="item in CRM_CP_RATING" :key="item" :label="item" :value="item"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
@@ -94,21 +112,23 @@
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_ST_RATING')">
               <el-select v-model="form.CRM_ST_RATING">
-                <el-option v-for="item in CRM_ST_RATING" :key="item"  :label="item"
-      :value="item"></el-option>
+                <el-option v-for="item in CRM_ST_RATING" :key="item" :label="item" :value="item"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <el-form-item :label="$t('table.CRM_LT_RATING')" label-width='150px'>
               <el-select v-model="form.CRM_LT_RATING">
-                <el-option v-for="item in CRM_LT_RATING" :key="item"  :label="item"
-      :value="item"></el-option>
+                <el-option v-for="item in CRM_LT_RATING" :key="item" :label="item" :value="item"></el-option>
               </el-select>
             </el-form-item>
           </el-col>
         </el-row>
       </el-form>
+      <div class="btnRow">
+        <el-button @click=" updateReleaseDia = false">{{$t('table.cancel')}}</el-button>
+        <el-button type="primary" @click="confirmUpdate(form)">{{$t('table.confirm')}}</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
@@ -119,38 +139,6 @@ export default {
   data() {
     return {
       updateReleaseDia: false,
-      releaseData: [
-        {
-          CRM_CODE: '',
-          CRM_TYPE: '银行秉兑汇票(非特别授权)(ZY030106)',
-          CCY_CODE: '人民币(CNY)',
-          CRM_VALUE_ORI: '1000',
-          CRM_CP_TYPE: '商品银行(BANK)',
-          CRM_CPC: '中华人民共和国(CHN)',
-          CRM_ST_RATING: '',
-          CRM_LT_RATING: ''
-        },
-        {
-          CRM_CODE: '',
-          CRM_TYPE: '银行秉兑汇票(非特别授权)(ZY030106)',
-          CCY_CODE: '人民币(CNY)',
-          CRM_VALUE_ORI: '1000',
-          CRM_CP_TYPE: '商品银行(BANK)',
-          CRM_CPC: '中华人民共和国(CHN)',
-          CRM_ST_RATING: '',
-          CRM_LT_RATING: ''
-        },
-        {
-          CRM_CODE: '',
-          CRM_TYPE: '银行秉兑汇票(非特别授权)(ZY030106)',
-          CCY_CODE: '人民币(CNY)',
-          CRM_VALUE_ORI: '1000',
-          CRM_CP_TYPE: '商品银行(BANK)',
-          CRM_CPC: '中华人民共和国(CHN)',
-          CRM_ST_RATING: '',
-          CRM_LT_RATING: ''
-        }
-      ],
       listQuery: {
         page: 1,
         limit: 20,
@@ -161,8 +149,7 @@ export default {
       },
       list: [],
       total: null,
-      form: {
-      },
+      form: {},
       startDate: '',
       endDate: '',
       CRM_TYPE: optionsTypeObj.CRM_TYPE,
@@ -186,6 +173,24 @@ export default {
       console.log(data)
       this.list = data.items
       this.total = data.total
+    },
+    confirmUpdate() {
+      this.updateReleaseDia = false
+      this.$notify({
+        title: '成功',
+        message: '修改成功',
+        type: 'success',
+        duration: 2000
+      })
+    },
+    deleteRow(index) {
+      this.list.splice(index, 1)
+      this.$notify({
+        title: '成功',
+        message: '删除成功',
+        type: 'success',
+        duration: 2000
+      })
     }
   },
   created() {
@@ -193,12 +198,15 @@ export default {
     this.startDate = new Date()
     this.endDate = new Date()
   }
-
 }
 </script>
 
 <style scoped>
 .el-table {
   padding: 20px;
+}
+.btnRow {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
